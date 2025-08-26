2f69fb3a6da954ac933b8ce312b9252a7d156e5a
Andrea Moretti <andrea.moretti1704@gmail.com>
2025-08-26 11:42:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// 🏆 Snack 1
+// Ottieni il titolo di un post con una Promise.
+
+// Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+// 🎯 Bonus: Ottieni l'intero post con l'autore
+
+const dumbLinkPost = "https://dummyjson.com/posts/"
+const dumbLinkUser = "https://dummyjson.com/users/"
+
+function getPostTitle (id){
+return new Promise((resolve,reject)=>{
+     fetch(`${dumbLinkPost}${id}`)
+    .then(res=> res.json())
+    .then(post =>resolve(post.title))
+    .catch(reject)
+})
+
+
+}getPostTitle(2)
+  .then(title => console.log(title))
+  .catch(err => console.error(err));
+// Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+// 🏆 Snack 2
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
